fd808076a0bb8fdec5b8edbf512712258acabd55
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-01-16 11:32:02 +0100

    Breaking

---
+++ Wolves&Farmers/Wolves&Farmers/Wolves_FarmersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wolves_FarmersApp.swift
+//  Wolves&Farmers
+//
+//  Created by Antonio Scognamiglio on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Wolves_FarmersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
