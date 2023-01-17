afd4f51f19124a138678daf525df66e9828804e1
simona2606 <simonaettari26@gmail.com>
2023-01-17 20:37:58 +0100

    Finish UsernameView and fix accent color

---
+++ WolvesFarmers/Utils/CustomTextField.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTextView.swift
+//  WolvesFarmers
+//
+//  Created by Simona Ettari on 17/01/23.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    
+    var placeholder: Text
+    @Binding var username: String
+    var editingChanged: (Bool)->() = { _ in }
+    var commit: ()->() = { }
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            if username.isEmpty { placeholder }
+            TextField("", text: $username, onEditingChanged: editingChanged, onCommit: commit).multilineTextAlignment(.center)
+        }
+    }
+    
+}
+
+struct CustomTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTextField(placeholder: Text("Insert your nickname"), username: .constant(""))
+    }
+}
