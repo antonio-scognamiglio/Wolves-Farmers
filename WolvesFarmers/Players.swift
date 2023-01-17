abfc28a6383f16b000d7dec8f7eb75b2f44b6b36
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-01-17 10:17:49 +0100

    Added Number of Players enum

---
+++ WolvesFarmers/Players.swift
@@ -0,0 +1,12 @@
+//
+//  Players.swift
+//  WolvesFarmers
+//
+//  Created by Antonio Scognamiglio on 17/01/23.
+//
+
+import Foundation
+
+enum NumberOfPlayers {
+    case six, seven, eight, nine, ten, eleven, twelve
+}
