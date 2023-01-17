d66e43cf6f13a0692c311cee59370d553b5c984b
alessia-andrisani <alessia.andrisani2@gmail.com>
2023-01-17 19:49:36 +0100

    Revert "Merge pull request #10 from antonio-scognamiglio/Search-Players"
    This reverts commit 0a5982cc4c9ae1f336e6fbcb7a0e56b31fd1255d, reversing
    changes made to f9ad7cccc257bde49e2c51c6a7f428a6622cb44f.

--- WolvesFarmers/View/PickSideView.swift
+++ WolvesFarmers/View/PickSideView.swift
@@ -10,26 +10,17 @@ import SwiftUI
 struct PickSideView: View {
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color.backgroundColor
-                    .ignoresSafeArea()
-                VStack {
-                    HeaderView(title: "Create a new game", subtitle: "Pick your role")
-                        .padding(.bottom, 100)
-                    Group {
-                        
-                        NavigationLink { SearchingPlayersView() } label: {
-                            RoleCardView(role: "Storyteller", imageName: "Storyteller")
-                                .foregroundColor(.black)
-                        }
-                        NavigationLink { Text("PlayerView") } label: {
-                            RoleCardView(role: "Player", imageName: "Players")
-                                .foregroundColor(.black)
-                        }
-                    }
-                    .padding()
+        ZStack {
+            Color.backgroundColor
+                .ignoresSafeArea()
+            VStack {
+                HeaderView(title: "Create a new game", subtitle: "Pick your role")
+                    .padding(.bottom, 100)
+                Group {
+                    RoleCardView(role: "Storyteller", imageName: "Storyteller")
+                    RoleCardView(role: "Player", imageName: "Players")
                 }
+                .padding()
             }
         }
     }
